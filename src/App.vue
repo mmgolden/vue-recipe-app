a015d37a8ef62edb1d30b63ba8476aeb91915bec
Melinda Golden <info@melindagolden.com>
2020-04-11 18:38:26 -0400

    add: Recipes to App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <div class="container">
+        <h1 class="title is-1">Recipes</h1>
+      </div>
+    </header>
+    <main>
+      <div class="container">
+        <recipe-form />
+        <recipes :recipes="recipes" />
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RecipeForm from "@/components/RecipeForm.vue";
+import Recipes from "@/components/Recipes.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    RecipeForm,
+    Recipes
+  },
+  data() {
+    return {
+      recipes: [
+        {
+          id: 1,
+          title: "🍪 Chocolate chip cookies",
+          ingredients:
+            "butter, sugar, vanilla extract, eggs, flour, baking soda, baking powder, salt, chocolate chips"
+        },
+        {
+          id: 2,
+          title: "🍌 Banana bread",
+          ingredients:
+            "flour, baking soda, salt, egg, sugar, vegetable oil, buttermilk, vanilla extract, bananas, walnuts"
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "~bulma/sass/utilities/_all";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Heebo", sans-serif;
+  font-size: 16px;
+  box-sizing: border-box;
 }
+
+.title {
+  padding: 3rem 2rem;
+}
+
+.container {
+  max-width: 700px !important;
+}
+
+$primary: #40a576;
+$primary-invert: findColorInvert($primary);
+
+$colors: (
+  "primary": (
+    $primary,
+    $primary-invert
+  )
+);
+
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
 </style>
