b7f26b7b386e8b489b793c6185b4f3de80c6bd14
Melinda Golden <info@melindagolden.com>
2020-04-11 18:37:46 -0400

    add: RecipeForm

---
+++ src/components/Recipes.vue
@@ -0,0 +1,40 @@
+<template>
+  <section id="recipes">
+    <div class="container">
+      <recipe-card
+        v-for="recipe in recipes"
+        :key="recipe.id"
+        :recipe="recipe"
+        class="recipe-card"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import RecipeCard from "@/components/RecipeCard.vue";
+
+export default {
+  name: "recipes",
+  components: {
+    RecipeCard
+  },
+  props: {
+    recipes: Array
+  }
+};
+</script>
+
+<style scoped lang="scss">
+#recipes {
+  padding: 0 2rem;
+}
+
+.recipe-card {
+  margin-bottom: 3rem;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+}
+</style>
